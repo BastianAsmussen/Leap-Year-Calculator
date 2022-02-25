55c2f919502c5806e684096b71f94962a8cb22ba
Bastian <76102128+BastianAsmussen@users.noreply.github.com>
2022-02-25 03:49:02 +0100

    Add files via upload

--- src/main/java/tech/asmussen/Main.java
+++ src/main/java/tech/asmussen/Main.java
@@ -1,10 +1,19 @@
 package tech.asmussen;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        boolean debugMode = false;
+
+        if (args.length > 0) {
+
+             debugMode = (args[0] != null) && (args[0].equalsIgnoreCase("debug"));
+        }
+
 
         Calculator calculator = new Calculator();
 
@@ -22,16 +31,21 @@ public class Main {
                 break;
             }
 
+            final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("###,###,###");
+
+            final long START_TIME = System.currentTimeMillis();
+
             int year = Integer.parseInt(input);
             boolean isLeapYear = calculator.isLeapYear(year);
 
-            if (isLeapYear) {
+            System.out.println("Year " + DECIMAL_FORMAT.format(year) + ((isLeapYear) ? " is " : " isn't ") + "a leap year!");
 
-                System.out.println("Year " + year + " is a leap year!");
+            if (debugMode) {
 
-            } else {
+                final double END_TIME = (System.currentTimeMillis() - START_TIME) / 1_000D;
+                final String[] TIME = String.valueOf(END_TIME).split("\\.");
 
-                System.out.println("Year " + year + " isn't a leap year!");
+                System.out.printf("Time: %s second(s) and %s millisecond(s).%n", TIME[0], TIME[1]);
             }
         }
     }
